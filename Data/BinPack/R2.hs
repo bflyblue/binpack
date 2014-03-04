72faf1e2aec6c2141ccb4b113c01382627faad83
Shaun Sharples <shaun.sharples@gmail.com>
2014-03-04 08:54:32 +0200

    merged in recent changes from jellyfish

--- Data/BinPack/R2.hs
+++ Data/BinPack/R2.hs
@@ -1,50 +1,80 @@
-module Data.BinPack.R2 where
+module Data.BinPack.R2
+( Bin2
+, pack
+, toList
+, sample
+, container
+, sqContainer
+, item
+, sqItem
+)
+where
 
 import Control.Applicative
-import Data.Foldable
+import Data.Foldable (Foldable, foldMap, foldlM)
 import Data.Monoid
+import Data.List
+import Data.Ord
 
-data Bin2 n a = B2Node
-              { binWidth, binHeight     :: n
-              , binRight, binBelow      :: Bin2 n a
-              , binItem                 :: a
-              }
-              | B2Empty
-              { binWidth, binHeight     :: n
-              }
+data Bin2 n a
+    = Node  { _width, _height   :: !n
+            , _right, _below    :: Bin2 n a
+            , _item             :: a }
+    | Free  { _width, _height   :: !n }
     deriving (Show)
 
+newtype Item n a = Item (n, n, a)
+
 instance Functor (Bin2 n) where
-    fmap _ (B2Empty w h) = B2Empty w h
-    fmap f (B2Node w h r b i) = B2Node w h (fmap f r) (fmap f b) (f i)
+    fmap f (Node w h r b e) = Node w h (fmap f r) (fmap f b) (f e)
+    fmap _ (Free w h)       = Free w h
 
 instance Foldable (Bin2 n) where
-    foldMap _ (B2Empty _ _) = mempty
-    foldMap f (B2Node _ _ r b i) = f i <> foldMap f r <> foldMap f b
+    foldMap f (Node _ _ r b e) = f e <> foldMap f r <> foldMap f b
+    foldMap _ (Free _ _)       = mempty
 
-rectBin :: n -> n -> Bin2 n a
-rectBin = B2Empty
+container :: n -> n -> Bin2 n a
+container = Free
 
-sqBin :: n -> Bin2 n a
-sqBin x = rectBin x x
+sqContainer :: n -> Bin2 n a
+sqContainer x = container x x
 
-pack :: (Num n, Ord n) => Bin2 n a -> n -> n -> a -> Maybe (Bin2 n a)
-pack (B2Node bw bh r b bi) w h i = right i <|> below i
-    where
-        right = fmap (\r' -> B2Node bw bh r' b bi) . pack r w h
-        below = fmap (\b' -> B2Node bw bh r b' bi) . pack b w h
-pack (B2Empty bw bh) w h i
-    | fits = Just $ B2Node w h right below i
+item :: n -> n -> a -> Item n a
+item w h i = Item (w, h, i)
+
+sqItem :: n -> a -> Item n a
+sqItem l = item l l
+
+pack' :: (Num n, Ord n) => Bin2 n a -> Item n a -> Maybe (Bin2 n a)
+pack' (Free fw fh) (Item (w, h, i))
+    | fits = Just $ Node w h pright pbelow i
     | otherwise = Nothing
     where
-        fits = w <= bw && h <= bh
-        right = B2Empty (bw - w) h
-        below = B2Empty bw (bh - h)
+        fits = w <= fw && h <= fh
+        pright = container (fw - w) h
+        pbelow = container fw (fh - h)
 
-sample :: (Num n, Ord n) => Bin2 n a -> n -> n -> Maybe a
-sample (B2Node bw bh r b i) x y =
-    case (compare x bw, compare y bh) of
-        (LT, LT) -> Just i
-        (_ , LT) -> sample r (x - bw) y
-        _        -> sample b x (y - bh)
-sample (B2Empty _ _) _ _ = Nothing
+pack' (Node nw nh r b ni) i = pright i <|> pbelow i
+    where
+        pright = fmap (\r' -> Node nw nh r' b ni) . pack' r
+        pbelow = fmap (\b' -> Node nw nh r b' ni) . pack' b
+
+pack :: (Num n, Ord n) => Bin2 n a -> [Item n a] -> Maybe (Bin2 n a)
+pack c = foldlM pack' c . sortBy (comparing area)
+    where
+        area (Item (w, h, _)) = w * h
+
+toList :: (Num n, Ord n) => Bin2 n a -> [(a, (n, n, n, n))]
+toList = unpack' 0 0
+    where
+        unpack' _ _ (Free _ _)       = []
+        unpack' x y (Node w h r b i) =
+            (i, (x, y, w, h)) : unpack' (x + w) y r ++ unpack' x (y + h) b
+
+sample :: (Num n, Ord n) => Bin2 n a -> n -> n -> Maybe (a, n, n)
+sample (Free _ _)       _ _ = Nothing
+sample (Node w h r b i) x y =
+    case (compare x w, compare y h) of
+        (LT, LT) -> Just (i, x, y)
+        (_ , LT) -> sample r (x - w) y
+        _        -> sample b x (y - h)
